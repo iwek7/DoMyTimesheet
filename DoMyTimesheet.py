786b1604d6775a26723af8858b6cd2b3f7aee8f6
iwek <miwanczuk7@gmail.com>
2021-08-30 01:32:55 +0200

    extract unique commit signatures

--- DoMyTimesheet.py
+++ DoMyTimesheet.py
@@ -1,4 +1,5 @@
 import os
+import re
 from functools import reduce
 from pathlib import Path
 
@@ -9,13 +10,28 @@ def flat_map(array):
     return reduce(lambda a, b: a + b, array)
 
 
+jira_signature_re = re.compile('([^s]+)')
+
 root = "/home/iwek/projects/DoMyTimesheetTests"
 all_directories = map(lambda path: root + "/" + path, os.listdir(root))
 git_repo_paths = filter(lambda sub_dir: Path(sub_dir + "/.git").exists(), all_directories)
 
-all_log_entries = []
+all_jira_signatures = []
 for repo_path in git_repo_paths:
     repo = git.Repo(repo_path)
-    all_log_entries += repo.git.log("--pretty='%s'").replace("'", "").split("\n")
+    # todo: take author from gitconfig
+    raw_log_entries = repo.git.log("--pretty='%s' --author 'iwek'").replace("'", "").split("\n")
+    jira_signatures = []
+    for log in raw_log_entries:
+        print(log)
+        split = re.search(r'[a-zA-Z]{3}-\d+', log)
+        if split:
+            jira_signatures.append(split.group(0))
+    all_jira_signatures += jira_signatures
 
-print(all_log_entries)
+unique_jira_signatures = set(all_jira_signatures)
+
+
+print(unique_jira_signatures)
+
+# git log --pretty
