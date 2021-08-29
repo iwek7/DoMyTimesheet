b0dbe4b9895e4e1bac87d36cbdbc5042d70f2cdb
iwek <miwanczuk7@gmail.com>
2021-08-29 21:31:17 +0200

    Extract git log lines

--- DoMyTimesheet.py
+++ DoMyTimesheet.py
@@ -1,8 +1,24 @@
 import os
+from functools import reduce
 from pathlib import Path
 
-root = "/home/iwek/projects/DoMyTimesheetTests"
-all_directories = os.listdir(root)
-git_repos = filter(lambda sub_dir: Path(root + "/" + sub_dir + "/.git").exists(), all_directories)
+import git
 
-print(list(git_repos))
+
+def flat_map(array):
+    return reduce(lambda a, b: a + b, array)
+
+
+root = "/home/iwek/projects/DoMyTimesheetTests"
+all_directories = map(lambda path: root + "/" + path, os.listdir(root))
+git_repo_paths = filter(lambda sub_dir: Path(sub_dir + "/.git").exists(), all_directories)
+
+all_log_entries = []
+for repo_path in git_repo_paths:
+    repo = git.Repo(repo_path)
+    logs_per_repo = repo.git.log("--pretty='%s'").replace("'", "").split("\n")
+
+    for l in logs_per_repo:
+        all_log_entries.append(l)
+
+print(all_log_entries)
