3a1481156c6e5f10014ce206a19be2f54047d477
iwek <miwanczuk7@gmail.com>
2021-08-30 02:08:39 +0200

    pretty print results

--- DoMyTimesheet.py
+++ DoMyTimesheet.py
@@ -1,29 +1,35 @@
 import os
 import re
-from functools import reduce
 from pathlib import Path
-
+from typing import Final
 import git
 
-
-def flat_map(array):
-    return reduce(lambda a, b: a + b, array)
-
-
-jira_signature_re = re.compile('([^s]+)')
-
+# user input
+start_date = "29-08-2021"
 root = "/home/iwek/projects/DoMyTimesheetTests"
+
+# constants
+JIRA_SIGNATURE_RE: Final = re.compile('([^s]+)')
+
+# program
+
+def print_results(unique_jira_signatures):
+    for res in unique_jira_signatures:
+        print(res)
+
 all_directories = map(lambda path: root + "/" + path, os.listdir(root))
 git_repo_paths = filter(lambda sub_dir: Path(sub_dir + "/.git").exists(), all_directories)
 
 all_jira_signatures = []
 for repo_path in git_repo_paths:
     repo = git.Repo(repo_path)
-    # todo: take author from gitconfig
-    raw_log_entries = repo.git.log("--pretty='%s' --author 'iwek'").replace("'", "").split("\n")
+    raw_log_entries = repo.git.log(
+        "--pretty='%s'",
+        "--after=\'" + start_date + " 00:00\'"
+        "--author 'iwek'"     # todo: take author from gitconfig
+    ).replace("'", "").split("\n")
     jira_signatures = []
     for log in raw_log_entries:
-        print(log)
         split = re.search(r'[a-zA-Z]{3}-\d+', log)
         if split:
             jira_signatures.append(split.group(0))
@@ -31,7 +37,4 @@ for repo_path in git_repo_paths:
 
 unique_jira_signatures = set(all_jira_signatures)
 
-
-print(unique_jira_signatures)
-
-# git log --pretty
+print_results(unique_jira_signatures)
